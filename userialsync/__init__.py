f03508046ce5c6b34edfd94534337bc8b9acbcdd
Jason Poindexter <poindexter.json@gmail.com>
2020-03-04 12:59:59 -0800

    WIP Implement dir map syncing

--- userialsync/__init__.py
+++ userialsync/__init__.py
@@ -1,26 +1,22 @@
 import os
 import subprocess
-
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
 from userialsync.debounce import debounce
 import time
 from pathlib import Path
-
 import argparse
+import ast
 
-# dir 
-# created - mkdir
-# modified - put
-# deleted - rmdir
 
-# file
-# created - put
-# modified - put
-# deleted - rm
+def dir_map(string):
+    return ast.literal_eval(string)
+
 
 # File to monitor TODO: make as an arg / default
 file_names = ['boot.py', 'main.py']
+
+
 # Paths = context path + filename
 # paths = []
 
@@ -42,9 +38,8 @@ class Main:
         self.root_ampy_cmd = f'ampy --port {self.port}'
         if args.baud is not None:
             self.root_ampy_cmd += f' --baud {self.baud}'
-        if args.directory_map is not None:
-            print(args.directory_map)
-            self.paths_map = self.directory_map_to_paths_map(args.directory_map)
+        if args.dir_map is not None:
+            self.paths_map = self.directory_map_to_paths_map(args.dir_map)
             print(self.paths_map)
 
         self.file_ops = {
@@ -58,19 +53,6 @@ class Main:
             'deleted': 'rmdir'
         }
         time.sleep(10)
-        event_handler = self.EventHandler()
-        observer = Observer()
-        observer.schedule(event_handler, self.root_path, recursive=True)
-        observer.start()
-        process = subprocess.run(['screen', '-S', session_name, args.port, args.baud], stdout=subprocess.PIPE,
-                                 stderr=subprocess.PIPE, universal_newlines=True)
-        print(process.stdout, process.stderr)
-        try:
-            while True:
-                time.sleep(1)
-        except KeyboardInterrupt:
-            observer.stop()
-        observer.join()
 
     # Turn file names to monitor into full paths using exe context
     @staticmethod
@@ -86,63 +68,86 @@ class Main:
     #             return path
 
     def directory_map_to_paths_map(self, directory_map):
-        print(len(directory_map))
+        results = []
         for directory in directory_map:
-            print(directory)
-            return [f'{directory[0]}/{self.root_path}', directory_map[1]]
+            results.append([f'{self.root_path}/{directory[0]}', directory[1]])
+        return results
+
+    def is_dir_map(self, src_path):
+        return any(src_path.find(path_map[0]) != -1 for path_map in self.paths_map)
 
     @debounce(1)
     def ampy_operation(self, src_path, operation):
         session_name = self.port.split('/')[2]  # ttyS8 / ttyS9 exc.
         ampy_cmd = f'{self.root_ampy_cmd} {operation} {src_path}'
         # Kill any running sessions
-        os.system(f'screen -S {session_name} -X quit')
+        # os.system(f'screen -S {session_name} -X quit')
         print(ampy_cmd)
-        if os.system(ampy_cmd) != 0:
-            return
+        # if os.system(ampy_cmd) != 0:
+        #     return
         print(session_name, self.port, self.baud)
-        os.system(f'screen -dmS {session_name} {self.port} {self.baud}')
+        # os.system(f'screen -dmS {session_name} {self.port} {self.baud}')
         # restart micropython machine
-        os.system(f'screen -S {session_name} -X stuff "^C^D"')
-        os.system(f'screen -r {session_name}')
+        # os.system(f'screen -S {session_name} -X stuff "^C^D"')
+        # os.system(f'screen -r {session_name}')
 
-    class EventHandler(LoggingEventHandler):
-        @staticmethod
-        def on_any_event(self, event):
-            if event.is_directory:
-                return
-            else:
-                if event.src_path == f'{os.getcwd()}/boot.py' or \
-                        event.src_path == f'{os.getcwd()}/main.py' or \
-                        event.src_path == self.directory_map:
-                    # Determine ampy operation to perform
-                    operation = self.file_op.get(event.event_type, None)
-                    if operation is not None:
-                        print(event.event_type, event.src_path)
-                        self.ampy_operation(event.src_path, operation)
-                    else:
-                        print(f'file operation {event.event_type} not supported')
-
-    def is_sync_path(self, event):
-        if event.is_directory:
-            return
-        else:
-            if event.src_path == f'{os.getcwd()}/boot.py' or \
-                    event.src_path == f'{os.getcwd()}/main.py' or \
-                    any(path_map[0].find(event.src_path) != -1 for path_map in self.paths_map):
-                operation = self.file_ops.get(event.event_type, None)
-                if operation is not None:
-                    print(event.event_type, event.src_path)
-                    self.ampy_operation(event.src_path, operation)
-                else:
-                    print('file operation {event_type} not supported')
+    # def is_sync_path(self, event):
+    #     if event.is_directory:
+    #         return
+    #     else:
+    #         if event.src_path == f'{os.getcwd()}/boot.py' or \
+    #                 event.src_path == f'{os.getcwd()}/main.py' or \
+    #                 any(path_map[0].find(event.src_path) != -1 for path_map in self.paths_map):
+    #             operation = self.file_ops.get(event.event_type, None)
+    #             if operation is not None:
+    #                 print(event.event_type, event.src_path)
+    #                 self.ampy_operation(event.src_path, operation)
+    #             else:
+    #                 print('file operation {event_type} not supported')
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', help='Serial port', type=str, required=True)
     parser.add_argument('--baud', help='Serial baud', type=str, required=True)
-    parser.add_argument('--directory_map', type=list, nargs='+', required=False)
+    # List of Lists [[*source_dir*, *dest_dir*]]that is used a a directory map to sync
+    # a local folder (and sub files/folders) to a micropython folder
+    # Example: ... --dir_map '[["public", "www"], '["libs", "modules"]']
+    # In this example the local project 'public' folder will sync to the 'www' folder on the micro python
+    # as well, the 'libs' folder will sync to the 'modules' folder
+    parser.add_argument('--dir_map', type=dir_map, required=False)
     args = parser.parse_args()
     session_name = args.port.split('/')[2]  # ttyS8 / ttyS9 exc.
-    Main(args, session_name)
+    userialsync = Main(args, session_name)
+
+    class EventHandler(LoggingEventHandler):
+        def on_any_event(self, event):
+            print(event.event_type, event.src_path)
+            print(userialsync.is_dir_map(event.src_path))
+
+            if event.is_directory:
+                return
+            else:
+                if event.src_path == f'{os.getcwd()}/boot.py' or \
+                        event.src_path == f'{os.getcwd()}/main.py':
+                    # Determine ampy operation to perform
+
+                    operation = userialsync.file_ops.get(event.event_type, None)
+                    if operation is not None:
+                        userialsync.ampy_operation(event.src_path, operation)
+                    else:
+                        print(f'file operation {event.event_type} not supported')
+
+    event_handler = EventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, userialsync.root_path, recursive=True)
+    observer.start()
+    # process = subprocess.run(['screen', '-S', session_name, args.port, args.baud], stdout=subprocess.PIPE,
+    #                          stderr=subprocess.PIPE, universal_newlines=True)
+    # print(process.stdout, process.stderr)
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
